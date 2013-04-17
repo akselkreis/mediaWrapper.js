76453e410b18cbd2d4d985a1cccb0cc2ca741878
Matthew A. K. Smith <matthew@akselkreismedia.com>
2013-04-16 21:44:22 -0400

    Added documentation.

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 #mediaWrapper.js
+## Simple responsive iframes.
 
+jQuery mediaWrapper is a plugin for wrapping an making iframes and thus embedded videos behave more like images when developing a responsive website. 
 
-jQuery mediaWrapper is a plugin for wrapping an making iframes and thus embedded videos behave like they should when developing a responsive website. 
+## Usage
 
-# Usage
+mediaWrapper accepts 2 options: baseHeight and baseWidth. These two options work together to determine the aspect ratio of the video you are embedding. You can use any combination of numbers, for example: an actual aspect ratio like '16 & 9' or '4 & 3' to set the aspect ration, or a set of pixel values such as '640' & '480'.
+
+<pre><code>
+$(document).ready(function(){
+	$('iframe').mediaWrapper({
+		baseWidth	:16, // 
+		baseHeight	:9
+	});
+});
+</code></pre>
+
+## Compatibility
+
+This plugin has been tested in the following browsers: 
+Firefox: 13, 20
+Chrome: 26
+Internet Explorer: 8
+
+License: MIT/GPLv2
--- jquery.mediaWrapper.js
+++ jquery.mediaWrapper.js
@@ -1,15 +1,19 @@
-(function( $ ){
+// mediaWrapper.js: Simple responsive iframes
+// MIT license http://www.opensource.org/licenses/mit-license.php/
+// @author Matthew A. K. Smith http://www.akselkreismedia.com
+
+ (function( $ ){
 
   $.fn.mediaWrapper = function(options) {
 
     var settings = $.extend( {
-      'baseWidth'   : 16,
-      'baseHeight'  : 9
+      baseWidth   : 16,
+      baseHeight  : 9
     }, options);
 
     return this.each(function() {
       var hRatio = (settings.baseHeight / settings.baseWidth) * 100;
-      $(this).wrap('<div class="mediaWrapper" style="max-width:'+ settings.maxWidth +''+ settings.maxWidthValue +'; position: relative; width: 100%; height: 0; padding: '+ hRatio +'% 0 0 0; " />').css("position","absolute").css("width", "100%").css("height", "100%").css("top", "0").css("left", "0");
+      $(this).wrap('<div class="mediaWrapper" style="position: relative; width: 100%; height: 0; padding: '+ hRatio +'% 0 0 0; " />').css("position","absolute").css("width", "100%").css("height", "100%").css("top", "0").css("left", "0");
     });
 
   };
